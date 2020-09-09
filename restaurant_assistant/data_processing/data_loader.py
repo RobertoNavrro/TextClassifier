e5a3b0c59933e66243aad5b4a164d86a4fcbfad5
RobertoNavrro <roberto.navrro@gmail.com>
2020-09-09 18:38:47 +0200

    Added BOW model for machine learning section

--- restaurant_assistant/data_processing/data_loader.py
+++ restaurant_assistant/data_processing/data_loader.py
@@ -4,7 +4,6 @@ from enum import Enum, auto
 from typing import Tuple
 import numpy
 
-
 class Column(Enum):
     label = auto()
     utterance = auto()
@@ -20,8 +19,32 @@ def load_dialog_data():
             tuppled_data.append([label, utterance])  # creates a tupple
     return tuppled_data
 
+def tokenize_utterance(string):
+    tokens = list()
+    tokens = string.split()
+    return tokens
+
+def generate_BOW_model():
+    utterance_list = list()
+    BOW = dict()
+    
+    path = str(Path(__file__).parent.parent.parent.joinpath('data', 'dialogs.dat'))
+    with open(path, "r") as raw_dialog_data:
+        for line in raw_dialog_data:
+            (label, utterance) = line.split(' ', 1)  # divides the data into two strings
+            utterance = utterance[:-1]
+            tokenized_utterance = tokenize_utterance(utterance)
+            for token in tokenized_utterance:
+                if token in BOW.keys():
+                    BOW[token]+= 1
+                if token not in BOW.keys():
+                    BOW[token] = 1
+    return BOW
 
 def generate_dataframes(tuppled_data) -> Tuple[pd.DataFrame, pd.DataFrame]:
     df = pd.DataFrame.from_records(tuppled_data, columns=[Column.label, Column.utterance])
     train_data, test_data = numpy.split(df, [int(0.8*len(df))])
     return train_data, test_data
+
+if __name__ == "__main__":
+    # Just for debugging :)
