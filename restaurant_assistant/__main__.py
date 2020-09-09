f343d6992638c091a42259dac101de4d8e72c5d0
Quintess.Barnhoorn@nslogin.nl <q.barnhoorn@student.ru.nl>
2020-09-09 12:22:48 +0200

    split train and test data in data_loader

--- restaurant_assistant/__main__.py
+++ restaurant_assistant/__main__.py
@@ -1,11 +1,11 @@
 from restaurant_assistant.data_processing import data_loader
 from restaurant_assistant.textclass.majority_classifier import MajorityClassifier
-from restaurant_assistant.textclass.keyword_classifier import KeywordClassifier
+
 
 def main():
-    df = data_loader.generate_dataframe(data_loader.load_dialog_data())
-    classifier = KeywordClassifier()
-    classifier.initialize(df)
+    train_data, test_data = data_loader.generate_dataframes(data_loader.load_dialog_data())
+    classifier = MajorityClassifier()
+    classifier.initialize(train_data)
 
     print('Welcome to the text classifier.')
     while(True):
@@ -13,6 +13,7 @@ def main():
         answer = input().lower()
         utterance_type = classifier.classify(answer)
         print(f'That utterance is of type {utterance_type.name}.')
+
 
 if __name__ == "__main__":
     main()
--- restaurant_assistant/data_processing/data_loader.py
+++ restaurant_assistant/data_processing/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 from enum import Enum, auto
+from typing import Tuple
+import numpy
 
 
 class Column(Enum):
@@ -11,14 +13,15 @@ class Column(Enum):
 def load_dialog_data():
     tuppled_data = list()
     path = str(Path(__file__).parent.parent.parent.joinpath('data', 'dialogs.dat'))
-    with open(path,"r") as raw_dialog_data:
+    with open(path, "r") as raw_dialog_data:
         for line in raw_dialog_data:
-            (label,utterance) = line.split(' ',1) #divides the data into two strings
-            utterance = utterance[:-1] #removes the \n character
-            tuppled_data.append([label, utterance]) #creates a tupple
+            (label, utterance) = line.split(' ', 1)  # divides the data into two strings
+            utterance = utterance[:-1]  # removes the \n character
+            tuppled_data.append([label, utterance])  # creates a tupple
     return tuppled_data
 
 
-def generate_dataframe(tuppled_data):
+def generate_dataframes(tuppled_data) -> Tuple[pd.DataFrame, pd.DataFrame]:
     df = pd.DataFrame.from_records(tuppled_data, columns=[Column.label, Column.utterance])
-    return df
+    train_data, test_data = numpy.split(df, [int(0.8*len(df))])
+    return train_data, test_data
