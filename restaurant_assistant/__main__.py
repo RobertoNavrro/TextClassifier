6db7df3e660e9dac54b0a343604b2499ec66d1f1
Quintess Barnhoorn <quintess.b@gmail.com>
2020-09-11 16:01:13 +0200

    Add performance measures

--- restaurant_assistant/__main__.py
+++ restaurant_assistant/__main__.py
@@ -1,13 +1,17 @@
 from restaurant_assistant.data_processing import data_loader
-from restaurant_assistant.textclass.majority_classifier import MajorityClassifier
-from restaurant_assistant.textclass.decision_tree_classifier import DecisionTreeClassifier
-from restaurant_assistant.textclass.keyword_classifier import KeywordClassifier
+from restaurant_assistant.textclass.neural_network_classifier import NeuralNetworkClassifier
 
 
 def main():
-    train_data, test_data = data_loader.generate_dataframes(data_loader.load_dialog_data())
-    classifier = MajorityClassifier()
+    data = data_loader.load_dialog_data()
+    train_data, test_data = data_loader.generate_dataframes(data)
+    classifier = NeuralNetworkClassifier()
     classifier.initialize(train_data)
+
+    print('Testing performance.')
+    f1_result, acc_result = classifier.test_performance(test_data)
+    print(f'F1 score: {f1_result}')
+    print(f'Accuracy: {acc_result}')
 
     print('Welcome to the text classifier.')
     while(True):
--- restaurant_assistant/textclass/utterance_classifier.py
+++ restaurant_assistant/textclass/utterance_classifier.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
 from enum import Enum, auto
 from pandas.core.frame import DataFrame
+from sklearn.metrics._classification import f1_score, accuracy_score
+
+from restaurant_assistant.data_processing.data_loader import Column
 
 
-"""
-Enumeration containing all possible types of utterances.
-"""
 class UtteranceType(Enum):
+    """
+    Enumeration containing all possible types of utterances.
+    """
     ack = auto()
     affirm = auto()
     bye = auto()
@@ -44,3 +47,18 @@ class UtteranceClassifier(ABC):
         """
         Classify the utterance as one of the utterance types and return the type.
         """
+
+    def test_performance(self, test_data: DataFrame):
+        """
+        Test the performance of the classifier on various metrics.
+        """
+        result = 'result'
+        test_data[result] = ''
+        for i in test_data.index:
+            guess = self.classify(test_data.at[i, Column.utterance])
+            test_data.at[i, result] = guess.name
+
+        f1_result = f1_score(test_data[Column.label], test_data[result], average='macro',
+                             labels=[x.name for x in UtteranceType], zero_division=1)
+        acc_result = accuracy_score(test_data[Column.label], test_data[result])
+        return f1_result, acc_result
